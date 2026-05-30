d17bdd93807bfad86f0bc1ead5d2c07bf9f845bf
theKiess <frank_kiess.junior@hotmail.com>
2026-05-29 22:16:23 -0300

    Primeiro commit - Fazendo configurações iniciais sobre o que terá...

---
+++ restaurante/config.php
@@ -0,0 +1,13 @@
+<?php
+  define("BASE_PATH", __DIR__);
+
+  require_once BASE_PATH . "/classes/Pessoa.php";
+  require_once BASE_PATH . "/classes/Cliente.php";
+  require_once BASE_PATH . "/classes/Funcionario.php";
+  require_once BASE_PATH . "/classes/Produto.php";
+  require_once BASE_PATH . "/classes/ItemPedido.php";
+  require_once BASE_PATH . "/classes/Pedido.php";
+  require_once BASE_PATH . "/includes/DataStore.php";
+
+  $isPages  = str_contains($_SERVER["SCRIPT_FILENAME"], "/pages/");
+  $basePath = $isPages ? "../" : "";
